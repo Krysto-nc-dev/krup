06150bd61908330ee5ad8ec93ef48a12adb15736
Velten Stoyann <krysto.dev@gmail.com>
2024-08-17 02:29:26 +1100

    initital commit and theme provider

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        { hostname: 'uploadthing.com' },
+        { hostname: 'utfs.io' },
+        { hostname: 'img.clerk.com' },
+        { hostname: 'subdomain' },
+        { hostname: 'files.stripe.com' },
+        { hostname: 'krystotest-erp.square.nc' },
+      ],
+    },
+    
+  }
+  
+  export default nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,184 @@
-import type { Config } from "tailwindcss";
+import { withUt } from 'uploadthing/tw';
+import colors from 'tailwindcss/colors';
 
-const config: Config = {
+module.exports = withUt({
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}', // Tremor module
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        tremor: {
+          brand: {
+            faint: '#E3F2FD',
+            muted: '#BBDEFB',
+            subtle: '#64B5F6',
+            DEFAULT: '#42A5F5',
+            emphasis: '#1E88E5',
+            inverted: colors.white,
+          },
+          background: {
+            muted: colors.gray[50],
+            subtle: colors.gray[100],
+            DEFAULT: '#edfdf9',  // Background from your charte graphique
+            emphasis: colors.gray[700],
+          },
+          border: {
+            DEFAULT: colors.gray[200],
+          },
+          ring: {
+            DEFAULT: colors.gray[200],
+          },
+          content: {
+            subtle: colors.gray[400],
+            DEFAULT: '#021611',  // Text color from your charte graphique
+            emphasis: colors.gray[700],
+            strong: colors.gray[900],
+            inverted: colors.white,
+          },
+        },
+        'dark-tremor': {
+          brand: {
+            faint: '#C5CAE9',
+            muted: '#7986CB',
+            subtle: '#5C6BC0',
+            DEFAULT: '#3F51B5',
+            emphasis: '#303F9F',
+            inverted: '#E8EAF6',
+          },
+          background: {
+            muted: '#1C2833',
+            subtle: '#2C3E50',
+            DEFAULT: '#34495E',
+            emphasis: '#ABB2B9',
+          },
+          border: {
+            DEFAULT: '#566573',
+          },
+          ring: {
+            DEFAULT: '#5D6D7E',
+          },
+          content: {
+            subtle: '#D5DBDB',
+            DEFAULT: '#ECF0F1',
+            emphasis: '#F8F9F9',
+            strong: '#FDFEFE',
+            inverted: '#17202A',
+          },
+        },
+        primary: {
+          DEFAULT: '#4bb39a', // Primary color from your charte graphique
+          foreground: '#ffffff',
+        },
+        secondary: {
+          DEFAULT: '#2196f3', // Secondary color from your charte graphique
+          foreground: '#ffffff',
+        },
+        accent: {
+          DEFAULT: '#ef5ce7', // Accent color from your charte graphique
+          foreground: '#ffffff',
+        },
+        destructive: {
+          DEFAULT: colors.red[600],
+          foreground: colors.white,
+        },
+        muted: {
+          DEFAULT: colors.gray[400],
+          foreground: colors.gray[600],
+        },
+        popover: {
+          DEFAULT: '#edfdf9',  // Background color from your charte graphique
+          foreground: '#021611',  // Text color from your charte graphique
+        },
+        card: {
+          DEFAULT: '#edfdf9',  // Background color from your charte graphique
+          foreground: '#021611',  // Text color from your charte graphique
+        },
+      },
+      boxShadow: {
+        // light
+        'tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+        'tremor-card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+        'tremor-dropdown': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+        // dark
+        'dark-tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+        'dark-tremor-card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+        'dark-tremor-dropdown': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+      },
+      borderRadius: {
+        'tremor-small': '0.375rem',
+        'tremor-default': '0.5rem',
+        'tremor-full': '9999px',
+      },
+      border: 'hsl(var(--border))',
+      input: 'hsl(var(--input))',
+      ring: 'hsl(var(--ring))',
+      background: 'hsl(var(--background))',
+      foreground: 'hsl(var(--foreground))',
+      primary: {
+        DEFAULT: 'hsl(var(--primary))',
+        foreground: 'hsl(var(--primary-foreground))',
+      },
+      secondary: {
+        DEFAULT: 'hsl(var(--secondary))',
+        foreground: 'hsl(var(--secondary-foreground))',
+      },
+      destructive: {
+        DEFAULT: 'hsl(var(--destructive))',
+        foreground: 'hsl(var(--destructive-foreground))',
+      },
+      muted: {
+        DEFAULT: 'hsl(var(--muted))',
+        foreground: 'hsl(var(--muted-foreground))',
+      },
+      accent: {
+        DEFAULT: 'hsl(var(--accent))',
+        foreground: 'hsl(var(--accent-foreground))',
+      },
+      popover: {
+        DEFAULT: 'hsl(var(--popover))',
+        foreground: 'hsl(var(--popover-foreground))',
+      },
+      card: {
+        DEFAULT: 'hsl(var(--card))',
+        foreground: 'hsl(var(--card-foreground))',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  safelist: [
+    {
+      pattern: /^(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ['hover', 'ui-selected'],
+    },
+    {
+      pattern: /^(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ['hover', 'ui-selected'],
+    },
+    {
+      pattern: /^(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ['hover', 'ui-selected'],
+    },
+    {
+      pattern: /^(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+    {
+      pattern: /^(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+    {
+      pattern: /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+  ],
+  plugins: [require('tailwindcss-animate')],
+});
