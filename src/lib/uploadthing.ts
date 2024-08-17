1a297de903b5d1533c378d05b4140542bab098d0
Velten Stoyann <krysto.dev@gmail.com>
2024-08-18 06:40:34 +1100

    upload thing hook

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,10 +1,10 @@
 import { generateUploadButton, generateUploadDropzone } from '@uploadthing/react';
 import type { OurFileRouter } from '@/app/api/uploadthing/core';
-import { useUploadThing } from '@uploadthing/react/hooks';
+import { generateReactHelpers } from '@uploadthing/react';
 
 // Création des composants UploadButton et UploadDropzone
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
 
-// Export de useUploadThing pour l'utilisation dans d'autres parties de l'application
-export { useUploadThing };
+// Génération des helpers React, y compris useUploadThing
+export const { useUploadThing } = generateReactHelpers<OurFileRouter>();
