b66cbac480c6d2f371e09fb3018ccca51f4f456b
Velten Stoyann <krysto.dev@gmail.com>
2024-08-18 16:26:11 +1100

    fixed error in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-import { withUt } from 'uploadthing/tw'
-import colors from 'tailwindcss/colors'
+import { withUt } from 'uploadthing/tw';
+import colors from 'tailwindcss/colors';
 
 module.exports = withUt({
   darkMode: ['class'],
@@ -15,15 +15,14 @@ module.exports = withUt({
       center: true,
       padding: '2rem',
       screens: {
-        
         '2xl': '1400px',
       },
     },
     extend: {
       colors: {
-         primary: '#4bb39a',
-         secondary: '#2196f3',
-         accent: '#ef5ce7',
+        primary: '#4bb39a',
+        secondary: '#2196f3',
+        accent: '#ef5ce7',
         tremor: {
           brand: {
             faint: '#E3F2FD',
@@ -82,59 +81,21 @@ module.exports = withUt({
             inverted: '#17202A',
           },
         },
-        boxShadow: {
-          // light
-          'tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
-          'tremor-card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
-          'tremor-dropdown': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
-          // dark
-          'dark-tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
-          'dark-tremor-card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
-          'dark-tremor-dropdown': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
-        },
-        borderRadius: {
-          'tremor-small': '0.375rem',
-          'tremor-default': '0.5rem',
-          'tremor-full': '9999px',
-        },
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
+      },
+      boxShadow: {
+        // light
+        'tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+        'tremor-card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+        'tremor-dropdown': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+        // dark
+        'dark-tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+        'dark-tremor-card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+        'dark-tremor-dropdown': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        'tremor-small': '0.375rem',
+        'tremor-default': '0.5rem',
+        'tremor-full': '9999px',
       },
       keyframes: {
         'accordion-down': {
@@ -181,4 +142,4 @@ module.exports = withUt({
     },
   ],
   plugins: [require('tailwindcss-animate')],
-})
+});
