e9c9800b0b27add7a9837136843330717e712eb8
Velten Stoyann <krysto.dev@gmail.com>
2024-08-20 05:56:58 +1100

    subaccount dashboard

--- src/app/(main)/subaccount/[subaccountId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/page.tsx
@@ -1,9 +1,169 @@
-import React from 'react';
+import BlurPage from '@/components/global/blur-page'
+import CircleProgress from '../../../../components/global/circle-progress'
+import PipelineValue from '../../../../components/global/pipeline-value'
+import SubaccountFunnelChart from '../../../../components/global/subaccount-funnel-chart'
+import { Badge } from '@/components/ui/badge'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
-type Props = {};
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { db } from '@/lib/db'
+import { AreaChart, BadgeDelta } from '@tremor/react'
+import { Contact2, ShoppingCart } from 'lucide-react'
+import React from 'react'
 
-const SubaccountPageId: React.FC<Props> = (props) => {
-    return <div>Sous-compte Page ID</div>;
+type Props = {
+  params: { subaccountId: string }
 }
 
-export default SubaccountPageId;
+const SubaccountPageId = async ({ params }: Props) => {
+  const subaccountDetails = await db.subAccount.findUnique({
+    where: {
+      id: params.subaccountId,
+    },
+  })
+
+  if (!subaccountDetails) return
+
+  const funnels = await db.funnel.findMany({
+    where: {
+      subAccountId: params.subaccountId,
+    },
+    include: {
+      FunnelPages: true,
+    },
+  })
+
+  const funnelPerformanceMetrics = funnels.map((funnel) => ({
+    ...funnel,
+    totalFunnelVisits: funnel.FunnelPages.reduce(
+      (total, page) => total + page.visits,
+      0
+    ),
+  }))
+
+  return (
+    <BlurPage>
+      <div className="relative h-full">
+        <div className="flex flex-col gap-4 pb-6">
+          <div className="flex gap-4 flex-col xl:!flex-row">
+            <PipelineValue subaccountId={params.subaccountId} />
+
+            <Card className="xl:w-fit">
+              <CardHeader>
+                <CardDescription>Conversions</CardDescription>
+                <CircleProgress
+                  value={0} // Remplacer avec la valeur réelle si nécessaire
+                  description={
+                    <>
+                      <div className="flex flex-col">
+                        Total Carts Opened
+                        <div className="flex gap-2">
+                          <ShoppingCart className="text-rose-700" />
+                          {/* Remplacer avec la valeur réelle si nécessaire */}
+                          0
+                        </div>
+                      </div>
+                      <div className="flex flex-col">
+                        Won Carts
+                        <div className="flex gap-2">
+                          <ShoppingCart className="text-emerald-700" />
+                          {/* Remplacer avec la valeur réelle si nécessaire */}
+                          0
+                        </div>
+                      </div>
+                    </>
+                  }
+                />
+              </CardHeader>
+            </Card>
+          </div>
+
+          <div className="flex gap-4 flex-col xl:!flex-row">
+            <Card className="relative">
+              <CardHeader>
+                <CardDescription>Funnel Performance</CardDescription>
+              </CardHeader>
+              <CardContent className="text-sm text-muted-foreground flex flex-col gap-12 justify-between">
+                <SubaccountFunnelChart data={funnelPerformanceMetrics} />
+                <div className="lg:w-[150px]">
+                  Total page visits across all funnels. Hover over to get more
+                  details on funnel page performance.
+                </div>
+              </CardContent>
+              <Contact2 className="absolute right-4 top-4 text-muted-foreground" />
+            </Card>
+            <Card className="p-4 flex-1">
+              <CardHeader>
+                <CardTitle>Checkout Activity</CardTitle>
+              </CardHeader>
+              <AreaChart
+                className="text-sm stroke-primary"
+                data={[]} // Remplacer avec les données réelles si nécessaire
+                index="created"
+                categories={['amount_total']}
+                colors={['primary']}
+                yAxisWidth={30}
+                showAnimation={true}
+              />
+            </Card>
+          </div>
+          <div className="flex gap-4 xl:!flex-row flex-col">
+            <Card className="p-4 flex-1 h-[450px] overflow-scroll relative">
+              <CardHeader>
+                <CardTitle className="flex items-center gap-2">
+                  Transition History
+                  <BadgeDelta
+                    className="rounded-xl bg-transparent"
+                    deltaType="moderateIncrease"
+                    isIncreasePositive={true}
+                    size="xs"
+                  >
+                    +12.3%
+                  </BadgeDelta>
+                </CardTitle>
+                <Table>
+                  <TableHeader className="!sticky !top-0">
+                    <TableRow>
+                      <TableHead className="w-[300px]">Email</TableHead>
+                      <TableHead className="w-[200px]">Status</TableHead>
+                      <TableHead>Created Date</TableHead>
+                      <TableHead className="text-right">Value</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody className="font-medium truncate">
+                    {/* Remplacer avec les données réelles si nécessaire */}
+                    <TableRow>
+                      <TableCell>-</TableCell>
+                      <TableCell>
+                        <Badge className="bg-emerald-500 dark:text-black">
+                          Paid
+                        </Badge>
+                      </TableCell>
+                      <TableCell>-</TableCell>
+                      <TableCell className="text-right">-</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </CardHeader>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </BlurPage>
+  )
+}
+
+export default SubaccountPageId
--- src/components/unauthorized/index.tsx
+++ src/components/unauthorized/index.tsx
@@ -1,125 +1,26 @@
-'use client'
-import { NotificationWithUser } from '@/lib/types'
-import { UserButton } from '@clerk/nextjs'
-import React, { useState } from 'react'
-import { twMerge } from 'tailwind-merge'
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '../ui/sheet'
-import { Bell } from 'lucide-react'
-import { Role } from '@prisma/client'
-import { Card } from '../ui/card'
-import { Switch } from '../ui/switch'
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
-import { ModeToggle } from '../global/mode-toggle'
+import Link from 'next/link'
+import React from 'react'
 
-type Props = {
-  notifications: NotificationWithUser | []
-  role?: Role
-  className?: string
-  subAccountId?: string
-}
+type Props = {}
 
-const InfoBar = ({ notifications, subAccountId, className, role }: Props) => {
-  const [allNotifications, setAllNotifications] = useState(notifications)
-  const [showAll, setShowAll] = useState(true)
-
-  const handleClick = () => {
-    if (!showAll) {
-      setAllNotifications(notifications)
-    } else {
-      if (notifications?.length !== 0) {
-        setAllNotifications(
-          notifications?.filter((item) => item.subAccountId === subAccountId) ??
-            []
-        )
-      }
-    }
-    setShowAll((prev) => !prev)
-  }
-
+const Unauthorized = (props: Props) => {
   return (
-    <>
-      <div
-        className={twMerge(
-          'fixed z-[20] md:left-[300px] left-0 right-0 top-0 p-4 bg-background/80 backdrop-blur-md flex  gap-4 items-center border-b-[1px] ',
-          className
-        )}
+    <div className="p-4 text-center h-screen w-screen flex justify-center items-center flex-col">
+      <h1 className="text-3xl md:text-6xl">Accès non autorisé!</h1>
+      <p>Oups! On dirait que vous avez pris un mauvais virage.</p>
+      <p>Veuillez contacter le support ou le propriétaire de l'entreprise pour obtenir l'accès.</p>
+      <Link
+        href="/"
+        className="mt-4 bg-primary p-2 rounded text-white"
       >
-        <div className="flex items-center gap-2 ml-auto">
-          <UserButton afterSignOutUrl="/" />
-          <Sheet>
-            <SheetTrigger>
-              <div className="rounded-full w-9 h-9 bg-primary flex items-center justify-center text-white">
-                <Bell size={17} />
-              </div>
-            </SheetTrigger>
-            <SheetContent className="mt-4 mr-4 pr-4 overflow-scroll">
-              <SheetHeader className="text-left">
-                <SheetTitle>Notifications</SheetTitle>
-                <SheetDescription>
-                  {(role === 'AGENCY_ADMIN' || role === 'AGENCY_OWNER') && (
-                    <Card className="flex items-center justify-between p-4">
-                      Current Subaccount
-                      <Switch onCheckedChange={handleClick} />
-                    </Card>
-                  )}
-                </SheetDescription>
-              </SheetHeader>
-              {allNotifications?.map((notification) => (
-                <div
-                  key={notification.id}
-                  className="flex flex-col gap-y-2 mb-2 overflow-x-scroll text-ellipsis"
-                >
-                  <div className="flex gap-2">
-                    <Avatar>
-                      <AvatarImage
-                        src={notification.User.avatarUrl}
-                        alt="Profile Picture"
-                      />
-                      <AvatarFallback className="bg-primary">
-                        {notification.User.name.slice(0, 2).toUpperCase()}
-                      </AvatarFallback>
-                    </Avatar>
-                    <div className="flex flex-col">
-                      <p>
-                        <span className="font-bold">
-                          {notification.notification.split('|')[0]}
-                        </span>
-                        <span className="text-muted-foreground">
-                          {notification.notification.split('|')[1]}
-                        </span>
-                        <span className="font-bold">
-                          {notification.notification.split('|')[2]}
-                        </span>
-                      </p>
-                      <small className="text-xs text-muted-foreground">
-                        {new Date(notification.createdAt).toLocaleDateString()}
-                      </small>
-                    </div>
-                  </div>
-                </div>
-              ))}
-              {allNotifications?.length === 0 && (
-                <div
-                  className="flex items-center justify-center text-muted-foreground"
-                  mb-4
-                >
-                  You have no notifications
-                </div>
-              )}
-            </SheetContent>
-          </Sheet>
-          <ModeToggle />
-        </div>
+        Retour à l'accueil 
+      </Link>
+      <div className="mt-8">
+        <img   src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdHYyaWY4OGhxYTFudHl2cmMybXY0MDB2Z2IxcG5mNWUybTJydDIybCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7uU59bOjkGMT53Zyhd/giphy.webp" alt="Unauthorized Cat" className="w-84 h-64" />
+        {/* <p className="italic mt-2">Même ce chat n'a pas accès...</p> */}
       </div>
-    </>
+    </div>
   )
 }
 
-export default InfoBar
+export default Unauthorized
